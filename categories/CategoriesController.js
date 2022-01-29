5b9dba06cc50d82cccdf960670d55287842bc6d3
Thaís Silva <thaiscaroline.cms@gmail.com>
2022-01-29 19:12:27 -0300

    delete and edit categories

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("./Category");
+const slugify = require("slugify");
 
 
 router.get("/categories", (req, res) => {
@@ -9,6 +11,61 @@ router.get("/categories", (req, res) => {
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new");
 })
+
+router.post("/categories/save", (req, res) => {
+    var title = req.body.title;
+    if(title != undefined){
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect("/admin/categories");
+        })
+    }else{
+        res.redirect("/admin/categories/new");
+    }
+});
+
+router.get("/admin/categories", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", {categories: categories});
+    });   
+});
+
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id;
+    if(id != undefined){
+        if(!isNaN){
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            });
+    }else{ //Not a number
+        res.redirect("/admin/categories");
+    }
+    }else{ //Null
+        res.redirect("/admin/categories");
+    }
+});
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    var id = req.params.id;
+    if(isNaN(id)){
+        res.redirect("/admin/categories");
+    }
+    Category.findByPk(id).then(category => {
+        if(category != undefined){
+            res.render("admin/categories/edit", {category: category});
+        }else{
+            res.redirect("/admin/categories");
+        }
+    }).catch(error => {
+        res.redirect("/admin/categories");
+    })
+});
 
 
 
